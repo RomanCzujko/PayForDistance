053930439d74722bfb30bfd4a227e9e61c4e2212
RomanCzujko <roman.chujko@gmail.com>
2020-10-09 15:24:33 +0300

    v0.1

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ $(function() {
         var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
         var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
 
+                var fixEutocompleteInterval = window.setInterval(function(){
+                var $container = $('body > .pac-container');
+                if ($container.length == 0) return;
+                // Move the autocomplete element just below the input.
+                $container.appendTo($('#origin').parent());
+                // The fix is finished, stop working.
+                window.clearInterval(fixEutocompleteInterval);
+            }, 500);
+
+            var fixEutocompleteInterval = window.setInterval(function(){
+                var $container = $('body > .pac-container');
+                if ($container.length == 0) return;
+                // Move the autocomplete element just below the input.
+                $container.appendTo($('#destination').parent());
+                // The fix is finished, stop working.
+                window.clearInterval(fixEutocompleteInterval);
+            }, 500);
+
         google.maps.event.addListener(from_places, 'place_changed', function () {
             var from_place = from_places.getPlace();
             var from_address = from_place.formatted_address;
