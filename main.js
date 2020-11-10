63cb0121f202f7a7817112da5e1afc8b9c269acb
RomanCzujko <roman.chujko@gmail.com>
2020-11-10 01:38:24 +0100

    v1.00

--- main.js
+++ main.js
@@ -272,6 +272,55 @@ $(mobile = function() {
    
 });
     
+$(mobile2 = function() {
+    // add input listeners
+    google.maps.event.addDomListener(window, 'focus', function () {
+        var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
+        var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
+
+        google.maps.event.addListener(from_places, 'place_changed', function () {
+            var from_place = from_places.getPlace();
+            var from_address = from_place.formatted_address;
+            $('#origin').val(from_address);
+        });
+
+        google.maps.event.addListener(to_places, 'place_changed', function () {
+            var to_place = to_places.getPlace();
+            var to_address = to_place.formatted_address;
+            $('#destination').val(to_address);
+        });
+
+    });
+
+    
+
+   
+});
+
+$(mobile3 = function() {
+    // add input listeners
+    google.maps.event.addDomListener(window, 'keyup', function () {
+        var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
+        var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
+
+        google.maps.event.addListener(from_places, 'place_changed', function () {
+            var from_place = from_places.getPlace();
+            var from_address = from_place.formatted_address;
+            $('#origin').val(from_address);
+        });
+
+        google.maps.event.addListener(to_places, 'place_changed', function () {
+            var to_place = to_places.getPlace();
+            var to_address = to_place.formatted_address;
+            $('#destination').val(to_address);
+        });
+
+    });
+
+    
+
+   
+});
 
 function modalButtons() {
     const backForm = document.querySelector(".main-section").hidden = false;
